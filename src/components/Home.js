36d70875c500b82a5ceffba40c99857b09ecc965
Igor Rocha <igortice@gmail.com>
2019-01-14 15:36:32 -0300

    better

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Actions } from 'react-native-router-flux';
 
 class Home extends Component {
+  getProtectedQuote() {
+    Alert.alert('We will print a Chuck Norris quote');
+  }
+
+  userLogout() {
+    Actions.login();
+  }
+
   render() {
     return (
-      <View>
-        <Text>Home</Text>
+      <View style={ styles.container }>
+        <TouchableOpacity style={ styles.buttonWrapper } onPress={ this.getProtectedQuote }>
+          <Text style={ styles.buttonText }> Get Chuck Norris quote! </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={ styles.buttonWrapper } onPress={ this.userLogout }>
+          <Text style={ styles.buttonText }> Log out </Text>
+        </TouchableOpacity>
       </View>
     );
   }
 }
 
 export default Home;
+
+const styles = StyleSheet.create({});
